535e47c5ccdd76181dec558bf3d24d26a7241cb8
Gerrald Mitchell <bluelionjr@gmail.com>
2017-04-20 07:14:07 -0400

    Checkpoint 16 Fake Data

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,8 @@ module Bloccit
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # Is line 25 necessary?  I added it here in case something breaks
+    # config.active_record.raise_in_transactional_callbacks = true
+    config.autoload_paths << File.join(config.root, "lib")
   end
 end
