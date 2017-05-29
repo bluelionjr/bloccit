a6e8beb933617a620a74b5e4cb5a2e244c8ffdf4
Gerrald Mitchell <bluelionjr@gmail.com>
2017-05-29 08:26:12 -0400

    Assignment 28 Complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -51,7 +51,16 @@ admin = User.create!(
 member = User.create!(
   name:     'Member User',
   email:    'member@example.com',
-  password: 'helloworld'
+  password: 'helloworld',
+  role:     'member'
+)
+
+# Create moderator
+moderator = User.create!(
+  name:     'Moderator User',
+  email:    'moderator@example.com',
+  password: 'helloworld',
+  role:     'moderator'
 )
 
 puts "Seed finished"
